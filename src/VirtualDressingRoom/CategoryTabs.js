545fe62832969bccceb0384c5d4dc5fd31b05f83
camha91 <crystalhanguyen@gmail.com>
2021-05-11 20:47:35 -0700

    Adding background

---
+++ src/VirtualDressingRoom/CategoryTabs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Tab from "./Tab";
+
+export default function CategoryTabs() {
+  return (
+    <div>
+      <Tab />
+    </div>
+  );
+}
---
+++ src/VirtualDressingRoom/Tab.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Tab() {
+  return <div>123</div>;
+}
