9055bd12e83e12f481a93a4f10cd5ecf8a104d83
camha91 <crystalhanguyen@gmail.com>
2021-05-11 21:40:41 -0700

    Create components

--- src/VirtualDressingRoom/CategoryTabs.js
+++ src/VirtualDressingRoom/CategoryTabs.js
@@ -1,10 +1,59 @@
 import React from "react";
-import Tab from "./Tab";
+import TabContent from "./TabContent";
 
 export default function CategoryTabs() {
   return (
-    <div>
-      <Tab />
+    <div className="col-md-8">
+      <ul className="nav nav-pills">
+        <li className="nav-item">
+          <a
+            className="nav-link active btn-default"
+            data-toggle="pill"
+            href="#tabTopClothes"
+          >
+            TOPS
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className="nav-link btn-default"
+            data-toggle="pill"
+            href="#tabBotClothes"
+          >
+            PANTS
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="pill" href="#tabShoes">
+            SHOES
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="pill" href="#tabHandBags">
+            BAGS
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="pill" href="#tabNecklaces">
+            ACCESSORIES
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="pill" href="#tabHairStyle">
+            HAIR STYLES
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" data-toggle="pill" href="#tabBackground">
+            BACKGROUND
+          </a>
+        </li>
+      </ul>
+
+      <div className="well">
+        {/* <!-- Tab panes --> */}
+        <TabContent />
+      </div>
     </div>
   );
 }
---
+++ src/VirtualDressingRoom/DressingTops.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function DressingTops() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <img src="assets/images/clothes/topcloth1_show.jpg" />
+            <h4>
+              <b>Top Cloth 1</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
