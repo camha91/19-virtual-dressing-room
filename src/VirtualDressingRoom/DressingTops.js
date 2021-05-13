9bf3200048e92b61d46fd46e281aeae85c3f0a38
camha91 <crystalhanguyen@gmail.com>
2021-05-12 17:05:25 -0700

    Render dressing top items

--- src/VirtualDressingRoom/DressingTops.js
+++ src/VirtualDressingRoom/DressingTops.js
@@ -1,64 +1,26 @@
 import React from "react";
+import dataDressing from "../Data/Data.json";
 
 export default function DressingTops() {
+  const renderTops = () => {
+    return dataDressing.tabPanes[0].tops.map((top, index) => {
+      console.log(dataDressing.tabPanes);
+      return (
+        <div key={index} className="col-md-4">
+          <div className="card text-center">
+            <img src={top.imgSrc_jpg} alt={top.imgSrc_jpg} />
+            <h4>
+              <b>{top.desc}</b>
+            </h4>
+            <button>Try on</button>
+          </div>
+        </div>
+      );
+    });
+  };
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card text-center">
-            <img src="assets/images/clothes/topcloth1_show.jpg" />
-            <h4>
-              <b>Top Cloth 1</b>
-            </h4>
-            <button>Try on</button>
-          </div>
-        </div>
-      </div>
+      <div className="row">{renderTops()}</div>
     </div>
   );
 }
